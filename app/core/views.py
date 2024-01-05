ca2e5e126504978440542c5c470b3eb871f58b49
Cesar <ccordero.007@gmail.cim>
2024-01-04 21:38:43 -0600

    Home, About, Porfolio and Contact pages added

--- app/core/views.py
+++ app/core/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+    <h1>My personal web</h1>
+    <ul>
+        <li><a href="/">Front Page</a></li>
+        <li><a href="/about-me/">About me</a></li>
+        <li><a href="/porfolio/">Porfolio</a></li>
+        <li><a href="/contact/">Contanct</a></li>
+    </ul>
+"""
+
 # Create your views here.
 def home(request):
-    html_response = "<h1>My personal web</h1>"
+    html_response = html_base + "<h1>My personal web</h1>"
     for i in range(10):
         html_response += "<h2>Por</h2>"
     return HttpResponse(html_response)
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>About me</h2>
+        <p>My name is Cesar and I am programmer</p>
+    """)
+
+def porfolio(request):
+    return HttpResponse(html_base + """
+        <h2>Porfolio</h2>
+        <p>Some of my projects</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contanct</h2>
+        <p>Here is my email if you have questions: <a href="mailto:test@test.test">test@test.test</a></p>
+    """)
