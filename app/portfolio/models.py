1d37e138a45f04c107b1c47a5d3f6c2e58b014be
Cesar <ccordero.007@gmail.cim>
2024-01-05 11:33:39 -0600

    Add portfolio app in admin panel, improve project model, and config url media for images

--- app/portfolio/models.py
+++ app/portfolio/models.py
@@ -4,6 +4,14 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to="projects")
     created = models.DateTimeField(auto_now_add=True) #This execute once time
     updated = models.DateTimeField(auto_now=True) #This execute each update time
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Project"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
