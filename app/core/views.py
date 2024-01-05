fa8375de06455468a40360047d7348c08e3f11b6
Cesar <ccordero.007@gmail.cim>
2024-01-04 21:25:35 -0600

    Testing urls and views for static pages

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    html_response = "<h1>My personal web</h1>"
+    for i in range(10):
+        html_response += "<h2>Por</h2>"
+    return HttpResponse(html_response)
