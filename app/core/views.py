bcf1c6dc60173553a333fdef2c4657be39a23f12
Cesar <ccordero.007@gmail.cim>
2024-01-04 21:49:14 -0600

    Using template instead of raw code

--- app/core/views.py
+++ app/core/views.py
@@ -12,25 +12,29 @@ html_base = """
 
 # Create your views here.
 def home(request):
-    html_response = html_base + "<h1>My personal web</h1>"
-    for i in range(10):
-        html_response += "<h2>Por</h2>"
-    return HttpResponse(html_response)
+    # html_response = html_base + "<h1>My personal web</h1>"
+    # for i in range(10):
+    #     html_response += "<h2>Por</h2>"
+    # return HttpResponse(html_response)
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>About me</h2>
-        <p>My name is Cesar and I am programmer</p>
-    """)
+    # return HttpResponse(html_base + """
+    #     <h2>About me</h2>
+    #     <p>My name is Cesar and I am programmer</p>
+    # """)
+    return render(request, "core/about.html")
 
 def porfolio(request):
-    return HttpResponse(html_base + """
-        <h2>Porfolio</h2>
-        <p>Some of my projects</p>
-    """)
+    # return HttpResponse(html_base + """
+    #     <h2>Porfolio</h2>
+    #     <p>Some of my projects</p>
+    # """)
+    return render(request, "core/porfolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contanct</h2>
-        <p>Here is my email if you have questions: <a href="mailto:test@test.test">test@test.test</a></p>
-    """)
+    # return HttpResponse(html_base + """
+    #     <h2>Contanct</h2>
+    #     <p>Here is my email if you have questions: <a href="mailto:test@test.test">test@test.test</a></p>
+    # """)
+    return render(request, "core/contact.html")
